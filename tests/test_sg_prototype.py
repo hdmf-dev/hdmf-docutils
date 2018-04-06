fec99c149166ffa23f8c105ae71effd47a5bcdeb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-04-06 02:51:22 -0400

    test: Fix and simplify test_sg_prototype
    * Reference "sg_prototype" from "nwb_docutils" instead of invalid
      "pynwb.docutils" package
    
    * Simplify test using built-in "tmpdir" fixture.
      See https://docs.pytest.org/en/latest/builtin.html and
      https://docs.pytest.org/en/latest/reference.html?highlight=TempdirFactory#tmpdir

--- tests/test_sg_prototype.py
+++ tests/test_sg_prototype.py
@@ -1,19 +1,7 @@
-import shutil
-import os
-import pytest
-import tempfile
 
-from pynwb.docutils.sg_prototype import build
+from nwb_docutils.sg_prototype import build
 
 
-@pytest.fixture(scope="function")
-def tempdir():
-    temp_dir = tempfile.mkdtemp()
-    return temp_dir
-
-
-def test_build(tempdir):
-
-    tgt_dir = os.path.join(tempdir, '_html_sg-prototype_TEST')
-    build(None, tgt_dir=tgt_dir)
-    shutil.rmtree(tempdir)
+def test_build(tmpdir):
+    build(None, tgt_dir=str(tmpdir.join('_html_sg-prototype_TEST')))
+    tmpdir.remove(rec=1)
