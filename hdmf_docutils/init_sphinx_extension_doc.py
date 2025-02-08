d8fd98d3ebc388ec14a5ce658e84c79f8e62e1b8
Ryan Ly <rly@lbl.gov>
2025-02-07 21:32:08 -0800

    Remove get_html_theme_path (#89)

--- hdmf_docutils/init_sphinx_extension_doc.py
+++ hdmf_docutils/init_sphinx_extension_doc.py
@@ -398,7 +398,6 @@ add_function_parentheses = False
 
 # -- HTML sphinx options
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # LaTeX Sphinx options
 latex_elements = {
